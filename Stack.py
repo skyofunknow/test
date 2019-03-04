ac4857974d2d376613c382b47b42f3ec6de7bb26
skyofunknow <36448743+skyofunknow@users.noreply.github.com>
2019-03-04 22:39:06 +0800

    Add files via upload

---
+++ Stack.py
@@ -0,0 +1,23 @@
+#栈
+class Stack():
+    def __init__(self):
+        self.__items=[]#两个下划线，类的私有成员
+    
+    def size(self):
+        return len(self.items)
+    
+    def peek(self):
+        if self.size()==0:
+            print("None")
+        else:
+            return self.items[self.size()-1]
+    
+    def push(self,data):#压栈
+        self.items.append(data)
+        
+    def pop(self):#出栈
+        if self.size()==0:
+            print("None")
+        else:
+            return self.items.pop()
+        
